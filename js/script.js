85937545bc06666bde86c5a14f1d7913ba9b26b5
Mau224 <46903680+Mau224@users.noreply.github.com>
2019-02-25 16:24:03 +0300

    slaider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var link = document.querySelector(".write");
 var popup = document.querySelector(".pop-up");
 var close = popup.querySelector(".modal-close");
-
+var controls = document.querySelectorAll('.slide-control');
+var slide = document.querySelectorAll('.slide');
+var iter = 0;
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -11,5 +13,27 @@ var close = popup.querySelector(".modal-close");
       evt.preventDefault();
       popup.classList.remove("modal-show");
     });
+    setInterval(function() {
+      changeSlide(slide[iter])
+      }, 1000)
 
-  });
+    for (var i = 0; i < controls.length; i++) {
+      controls[i].onclick = function(evt) {
+      evt.preventDefault();
+      var j = this.getAttribute('data-slide');
+      changeSlide(slide[j])
+
+      }
+      }
+
+      function changeSlide(elem) {
+      for (var j = 0; j < slide.length; j++) {
+      slide[j].classList.remove('active');
+      }
+      elem.classList.add('active');
+      }
+      elem.classList.add('active');
+      iter++;
+      if (iter > 2) iter = 0;
+
+});
