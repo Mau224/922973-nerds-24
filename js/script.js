de2bf849c58aaadacce6b9759da6718b30d2b931
Mau224 <46903680+Mau224@users.noreply.github.com>
2019-03-16 17:16:59 +0300

    2 попытка

--- js/script.js
+++ js/script.js
@@ -4,20 +4,60 @@ var link = document.querySelector(".write");
 
 var popup = document.querySelector(".pop-up");
 var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var Name = popup.querySelector("[name=name]");
 var iter = 0;
 
+var isStorageSupport = true;
+var storage = "";
 
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
 
-    close.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popup.classList.remove("modal-show");
-    });
+    if (storage) {
+      Name.value = storage;
+      Login.focus();
+    } else {
+      Name.focus();
+    }
+  });
 
-});
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!login.value || !Name.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
 
 for (var i = 0; i < controls.length; i++) {
   controls[i].onclick = function(evt) {
@@ -39,127 +79,5 @@ for (var i = 0; i < controls.length; i++) {
   iter++;
   if (iter>2)iter=0;
   };
-function filterRangeSlider() {
-  var filterRange = document.querySelector(".filter-range");
-  var rangeSlider = document.querySelector(".range-slider");
-  var tdLeft = rangeSlider.querySelector(".range-slider__td_left");
-  var tdRight = rangeSlider.querySelector(".range-slider__td_right");
-  var thumbLeft = rangeSlider.querySelector(".range-slider__thumb_left");
-  var thumbRight = rangeSlider.querySelector(".range-slider__thumb_right");
-  var thumbWidth = thumbRight.offsetWidth;
-  var valueMin = filterRange.querySelector(".filter-range__value_min");
-  var valueMax = filterRange.querySelector(".filter-range__value_max");
-  var tableWidth = rangeSlider.offsetWidth;
-  var ratio = (valueMax.dataset.max_value - valueMin.dataset.min_value) / (tableWidth - thumbWidth * 2);
-
-    function getCoords(elem) {
-      var box = elem.getBoundingClientRect();
-
-      return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
-      };
-    }
-
-    function init() {
-      tdLeft.style.width = (Number(valueMin.value) / ratio + Number(thumbWidth)) + "px";
-      tdRight.style.width = (Number(tableWidth) - Number(valueMax.value) / ratio - Number(thumbWidth)) + "px";
-    }
-
-    thumbLeft.onmousedown = function (evt) {
-      var thumbCoords = getCoords(this);//Получаем координаты текущего thumb
-      var shiftX = evt.pageX - thumbCoords.left;//Расстояние от клика до левого края thumb
-      var sliderCoords = getCoords(tdLeft);//Получаем координаты левого td
-      var widthTdRight = tdRight.offsetWidth;//Получаем шинину правого td
-
-      document.onmousemove = function (evt) {
-        var newLeft = evt.pageX - sliderCoords.left;//Расстояние от дв.мыши до левого края tdLeft.
-        var widthTd = newLeft - shiftX + thumbWidth;//Расчёт ширины перед условиями
-
-        if (newLeft <= shiftX) {
-          widthTd = thumbWidth;
-        }
-        if (widthTd > tableWidth - widthTdRight) {
-          widthTd = tableWidth - widthTdRight;
-        }
-        tdLeft.style.width = widthTd + "px";
-        document.onmouseup = function () {
-          document.onmousemove = document.onmouseup = null;
-        };
-        valueMin.value = (tdLeft.offsetWidth - thumbWidth) * ratio;
-      };
-      return false;
-    };
-
-    thumbRight.onmousedown = function (evt) {
-      var thumbCoords = getCoords(this);
-      var shiftX = thumbWidth - (evt.pageX - thumbCoords.left);
-      var sliderCoordsX = getCoords(tdRight).left;
-      var widthTdLeft = tdLeft.offsetWidth;
-      var widthTdRight = tdRight.offsetWidth;
-
-      document.onmousemove = function (evt) {
-        var newRight = sliderCoordsX + widthTdRight - evt.pageX;
-        var widthTd = newRight - shiftX + thumbWidth;
-
-        if (newRight <= shiftX) {
-          widthTd = thumbWidth;
-        }
-        if (widthTd > tableWidth - widthTdLeft) {
-          widthTd = tableWidth - widthTdLeft;
-        }
-        tdRight.style.width = widthTd + "px";
-        document.onmouseup = function () {
-          document.onmousemove = document.onmouseup = null;
-        };
-        valueMax.value = (tableWidth - tdRight.offsetWidth - thumbWidth) * ratio;
-      };
-      return false;
-    };
-
-    valueMin.addEventListener("keydown", function (evt) {
-      var tdLeftMaxWidth = tableWidth - tdRight.offsetWidth;
-      if (evt.keyCode === 13 && this.focus) {
-        if (this.value < 0) {
-          this.value = 0;
-          tdLeft.style.width = thumbWidth + "px";
-        }
-        if (this.value > (tdLeftMaxWidth - thumbWidth) * ratio) {
-          this.value = (tdLeftMaxWidth - thumbWidth) * ratio;
-          tdLeft.style.width = tdLeftMaxWidth + "px";
-        }
-        if (0 <= this.value <= (tdLeftMaxWidth - thumbWidth) * ratio) {
-          tdLeft.style.width = (Number(this.value) / ratio + Number(thumbWidth)) + "px";
-        }
-        this.select();
-      }
-    });
-
-    valueMax.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 13 && this.focus) {
-        if (+this.value < +valueMin.value) {
-          this.value = valueMin.value;
-          tdRight.style.width = (Number(thumbWidth) - Number(tdLeft.offsetWidth)) + "px";
-        }
-        if (this.value > (tableWidth - thumbWidth * 2) * ratio) {
-          this.value = (tableWidth - thumbWidth * 2) * ratio;
-          tdRight.style.width = (Number(thumbWidth) - Number(thumbWidth)) + "px";
-        }
-        if (valueMin.value <= this.value <= (tableWidth - thumbWidth * 2) * ratio) {
-          tdRight.style.width = (Number(tableWidth) - Number(valueMax.value) / ratio - Number(thumbWidth)) + "px";
-        }
-        this.select();
-      }
-    });
-
-    thumbLeft.ondragstart = function () {
-      return false;
-    };
-
-    init();
-  }
-
-  filterRangeSlider();
-
 
 
